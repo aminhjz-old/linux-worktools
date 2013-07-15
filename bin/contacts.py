1a23df376d95b0ec19312c5619d86ffc9bfd8a0e
Amin Hedjazi <Ahejazi@kishware.com>
2013-07-15 12:50:04 +0430

    added a copletation script for extracting contacts from a csv file and a call script that uses this tool to call that contact by ekiga softphone

---
+++ bin/contacts.py
@@ -0,0 +1,66 @@
+#!/usr/bin/python
+import csv
+import optparse
+import re         
+from itertools import izip
+
+def print_contacts(contact,column):
+	result=""	
+	if column==None:
+		for key in contact.keys():
+			value = contact[key]
+			if value!=None and value != "":
+				result+="\n"
+				result+= key+" :"+value
+		result +="\n"		
+		result +="*****************************"
+		result +="\n"		
+
+	else:
+                result=contact[column]
+	return result
+
+def search(pattern,sfeild,contacts):
+	res=[]
+	for contact in contacts:
+		if (pattern.match(contact[sfeild])):
+			res.append(contact)
+	return res
+
+
+def loadcsv(csvfile):
+	contacts= csv.DictReader(csvfile, delimiter = ',', quotechar = '"')
+	return contacts
+
+def main():
+	p = optparse.OptionParser()
+        p.add_option('--query', '-q',default=["Last Name","First Name"])
+        p.add_option('--file', '-f')
+        p.add_option('--columns', '-c')
+
+        options, arguments = p.parse_args()
+
+	csvfile= open(options.file, 'rb')
+	if (len(arguments)<1):
+		return -1
+	results = loadcsv(csvfile)
+	for query,arg in zip(options.query,arguments):
+		prog = re.compile(arg)
+	       	results=search(prog,query,results)
+	if results==None:
+		return -1
+	if options.columns!=None:
+		columns = options.columns.split(",")
+	else:
+		columns =[None]
+	resultstr=[]
+	for result in results:
+		tmp=[]
+		for column in columns:
+			tmp.append(print_contacts(result,column))
+		resultstr.append(" ".join(tmp))
+			
+	print "\n ".join(resultstr)
+if __name__ == '__main__':
+	main()
+
